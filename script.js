72db2379bd48397dc81cad81d45efa3ec7e6787c
itxfatima021 <fatimaamanat75@gmail.com>
2024-07-23 05:09:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("sideNavmenu").style.width="250px";
+}
+function closeNav(){
+    document.getElementById("sideNavmenu").style.width="0";
+}
+function Opensearch(){
+    document.getElementById("side-search-box").style.width="350px";
+}
